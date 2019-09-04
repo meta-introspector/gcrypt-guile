25cb590921393b1dc924955ced2f10630828f134
Ludovic Courtès <ludo@gnu.org>
2019-09-04 23:11:36 +0200

    hash: Generalize procedures and expose all the algorithms.
    * gcrypt/hash.scm (define-enumerate-type, define-lookup-procedure)
    (define-hash-algorithms): New macros.
    (%hash-size): New procedure.
    (hash-algorithm): New macro.
    (lookup-hash-algorithm, hash-size): New procedures.
    (GCRY_MD_SHA256, GCRY_MD_SHA1): Remove.
    * gcrypt/hash.scm (bytevector-hash): Remove 'size' parameter and call
    'hash-size' instead.  Rename 'type' to 'algorithm'.
    (sha1, sha256): Adjust accordingly.
    (open-sha256-md): Rename to...
    (open-md): ... this, and add 'algorithm' parameter.
    (open-sha256-port): Rename to...
    (open-hash-port): ... this, and add 'algorithm' parameter.
    (port-sha256): Rename to...
    (port-hash): ... this, and add 'algorithm' parameter.
    (file-sha256): Rename to...
    (file-hash): ... this, and add 'algorithm' parameter.
    (open-sha256-input-port): Rename to...
    (open-hash-input-port): ... this, and add 'algorithm' parameter.
    (open-sha256-port, port-sha256, file-sha256)
    (open-sha256-input-port): Redefine in terms of the generic procedures
    above.
    * tests/hash.scm (%hello-sha512): New variable.
    ("lookup-hash-algorithm", "hash-size"): New tests.
    ("sha512, empty", "sha512, hello")
    ("open-hash-port, sha512, hello")
    ("open-hash-input-port, sha512, hello"): New tests.
    * guile-gcrypt.texi (Hash Functions): Document it.
    
    Co-authored-by: Brendan Tildesley <mail@brendan.scot>

--- gcrypt/hash.scm
+++ gcrypt/hash.scm
@@ -25,7 +25,17 @@
   #:use-module (system foreign)
   #:use-module (srfi srfi-11)
   #:use-module (srfi srfi-26)
-  #:export (sha1
+  #:export (hash-algorithm
+            lookup-hash-algorithm
+            hash-size
+
+            bytevector-hash
+            open-hash-port
+            port-hash
+            file-hash
+            open-hash-input-port
+
+            sha1
             sha256
             open-sha256-port
             port-sha256
@@ -43,37 +53,133 @@
 ;;; Hash.
 ;;;
 
-(define-syntax GCRY_MD_SHA256
-  ;; Value as of Libgcrypt 1.5.2.
-  (identifier-syntax 8))
+(define-syntax-rule (define-enumerate-type name->integer symbol->integer
+                      (name id) ...)
+  (begin
+    (define-syntax name->integer
+      (syntax-rules (name ...)
+        "Return hash algorithm NAME."
+        ((_ name) id) ...))
 
-(define-syntax GCRY_MD_SHA1
-  (identifier-syntax 2))
+    (define symbol->integer
+      (let ((alist '((name . id) ...)))
+        (lambda (symbol)
+          "Look up SYMBOL and return the corresponding integer or #f if it
+could not be found."
+          (assq-ref alist symbol))))))
+
+(define-syntax define-lookup-procedure
+  (lambda (s)
+    "Define LOOKUP as a procedure that maps an integer to its corresponding
+value in O(1)."
+    (syntax-case s ()
+      ((_ lookup docstring (index value) ...)
+       (let* ((values (syntax->datum #'((index . value) ...)))
+              (min    (apply min (syntax->datum #'(index ...))))
+              (max    (apply max (syntax->datum #'(index ...))))
+              (array  (let loop ((i max)
+                                 (result '()))
+                        (if (< i (- min 1))
+                            result
+                            (loop (- i 1)
+                                  (cons (or (assv-ref values i) -1)
+                                        result))))))
+         #`(define lookup
+             ;; Allocate a big sparse vector.
+             (let ((values '#(#,@array)))
+               (lambda (integer)
+                 docstring
+                 (and (<= integer #,max) (> integer #,min)
+                      (let ((result (vector-ref values integer)))
+                        (and (> result 0) result)))))))))))
+
+(define-syntax-rule (define-hash-algorithms name->integer
+                      symbol->integer hash-size
+                      (name id size) ...)
+  "Define hash algorithms with their NAME, numerical ID, and SIZE in bytes."
+  (begin
+    (define-enumerate-type name->integer symbol->integer
+      (name id) ...)
+
+    (define-lookup-procedure hash-size
+      "Return the size in bytes of a digest of the given hash algorithm."
+      (id size) ...)))
+
+(define %hash-size
+  ;; This procedure was used to double-check the hash sizes below.  (We
+  ;; cannot use it at macro-expansion time because it wouldn't work when
+  ;; cross-compiling.)
+  (libgcrypt->procedure unsigned-int
+                        "gcry_md_get_algo_dlen"
+                        (list int)))
+
+;; 'GCRY_MD_' values as of Libgcrypt 1.8.3.
+(define-hash-algorithms hash-algorithm
+  lookup-hash-algorithm
+  hash-size
+
+  (md5 1 16)
+  (sha1 2 20)
+  (rmd160 3 20)
+  ;; (md2 5 0)
+  (tiger 6 24)   ;TIGER/192 as used by gpg <= 1.3.2
+  (haval 7 20)   ;HAVAL, 5 pass, 160 bit
+  (sha256 8 32)
+  (sha384 9 48)
+  (sha512 10 64)
+  (sha224 11 28)
+
+  (md4 301 16)
+  (crc32 302 4)
+  (crc32-rfc1510 303 4)
+  (crc24-rfc2440 304 3)
+  (whirlpool 305 64)
+  (tiger1 306 24) ;TIGER fixed
+  (tiger2 307 24) ;TIGER2 variant
+  (gostr3411-94 308 32) ;GOST R 34.11-94
+  (stribog256 309 32) ;GOST R 34.11-2012, 256 bit
+  (stribog512 310 64) ;GOST R 34.11-2012, 512 bit
+  (gostr3411-cp 311 32) ;GOST R 34.11-94 with CryptoPro-A S-Box
+  (sha3-224 312 28)
+  (sha3-256 313 32)
+  (sha3-384 314 48)
+  (sha3-512 315 64)
+  ;; (shake128 316 0)
+  ;; (shake256 317 0)
+  (blake2b-512 318 64)
+  (blake2b-384 319 48)
+  (blake2b-256 320 32)
+  (blake2b-160 321 20)
+  (blake2s-256 322 32)
+  (blake2s-224 323 28)
+  (blake2s-160 324 20)
+  (blake2s-128 325 16))
+
 
 (define bytevector-hash
   (let ((proc (libgcrypt->procedure void
                                     "gcry_md_hash_buffer"
                                     `(,int * * ,size_t))))
-    (lambda (bv type size)
-      "Return the hash TYPE, of SIZE bytes, of BV as a bytevector."
-      (let ((digest (make-bytevector size)))
-        (proc type (bytevector->pointer digest)
+    (lambda (bv algorithm)
+      "Return the hash ALGORITHM of BV as a bytevector."
+      (let ((digest (make-bytevector (hash-size algorithm))))
+        (proc algorithm (bytevector->pointer digest)
               (bytevector->pointer bv) (bytevector-length bv))
         digest))))
 
 (define sha1
-  (cut bytevector-hash <> GCRY_MD_SHA1 20))
+  (cut bytevector-hash <> (hash-algorithm sha1)))
 
 (define sha256
-  (cut bytevector-hash <> GCRY_MD_SHA256 (/ 256 8)))
+  (cut bytevector-hash <> (hash-algorithm sha256)))
 
-(define open-sha256-md
+(define open-md
   (let ((proc (libgcrypt->procedure int
                                     "gcry_md_open"
                                     `(* ,int ,unsigned-int))))
-    (lambda ()
+    (lambda (algorithm)
       (let* ((md  (bytevector->pointer (make-bytevector (sizeof '*))))
-             (err (proc md GCRY_MD_SHA256 0)))
+             (err (proc md algorithm 0)))
         (if (zero? err)
             (dereference-pointer md)
             (throw 'gcrypt-error err))))))
@@ -87,21 +193,23 @@
 (define md-close
   (libgcrypt->procedure void "gcry_md_close" '(*)))
 
-
-(define (open-sha256-port)
+(define (open-hash-port algorithm)
   "Return two values: an output port, and a thunk.  When the thunk is called,
-it returns the SHA256 hash (a bytevector) of all the data written to the
+it returns the hash (a bytevector) for ALGORITHM of all the data written to the
 output port."
-  (define sha256-md
-    (open-sha256-md))
+  (define md
+    (open-md algorithm))
+
+  (define md-size
+    (hash-size algorithm))
 
   (define digest #f)
   (define position 0)
 
   (define (finalize!)
-    (let ((ptr (md-read sha256-md 0)))
-      (set! digest (bytevector-copy (pointer->bytevector ptr 32)))
-      (md-close sha256-md)))
+    (let ((ptr (md-read md 0)))
+      (set! digest (bytevector-copy (pointer->bytevector ptr md-size)))
+      (md-close md)))
 
   (define (write! bv offset len)
     (if (zero? len)
@@ -109,7 +217,7 @@ output port."
           (finalize!)
           0)
         (let ((ptr (bytevector->pointer bv offset)))
-          (md-write sha256-md ptr len)
+          (md-write md ptr len)
           (set! position (+ position len))
           len)))
 
@@ -120,7 +228,7 @@ output port."
     (unless digest
       (finalize!)))
 
-  (values (make-custom-binary-output-port "sha256"
+  (values (make-custom-binary-output-port "hash"
                                           write! get-position #f
                                           close)
           (lambda ()
@@ -128,23 +236,28 @@ output port."
               (finalize!))
             digest)))
 
-(define (port-sha256 port)
-  "Return the SHA256 hash (a bytevector) of all the data drained from PORT."
+(define (port-hash algorithm port)
+  "Return the ALGORITHM hash (a bytevector) of all the data drained from
+PORT."
   (let-values (((out get)
-                (open-sha256-port)))
+                (open-hash-port algorithm)))
     (dump-port port out)
     (close-port out)
     (get)))
 
-(define (file-sha256 file)
-  "Return the SHA256 hash (a bytevector) of FILE."
-  (call-with-input-file file port-sha256))
+(define (file-hash algorithm file)
+  "Return the ALGORITHM hash (a bytevector) of FILE."
+  (call-with-input-file file
+    (cut port-hash algorithm <>)))
 
-(define (open-sha256-input-port port)
+(define (open-hash-input-port algorithm port)
   "Return an input port that wraps PORT and a thunk to get the hash of all the
 data read from PORT.  The thunk always returns the same value."
   (define md
-    (open-sha256-md))
+    (open-md algorithm))
+
+  (define md-size
+    (hash-size algorithm))
 
   (define (read! bv start count)
     (let ((n (get-bytevector-n! port bv start count)))
@@ -160,7 +273,7 @@ data read from PORT.  The thunk always returns the same value."
 
   (define (finalize!)
     (let ((ptr (md-read md 0)))
-      (set! digest (bytevector-copy (pointer->bytevector ptr 32)))
+      (set! digest (bytevector-copy (pointer->bytevector ptr md-size)))
       (md-close md)))
 
   (define (get-hash)
@@ -176,7 +289,17 @@ data read from PORT.  The thunk always returns the same value."
                           (else 'none)))
     port)
 
-  (values (unbuffered (make-custom-binary-input-port "sha256" read! #f #f #f))
+  (values (unbuffered (make-custom-binary-input-port "hash-input"
+                                                     read! #f #f #f))
           get-hash))
+
+(define open-sha256-port
+  (cut open-hash-port (hash-algorithm sha256)))
+(define port-sha256
+  (cut port-hash (hash-algorithm sha256) <>))
+(define file-sha256
+  (cut file-hash (hash-algorithm sha256) <>))
+(define open-sha256-input-port
+  (cut open-hash-input-port (hash-algorithm sha256) <>))
 
 ;;; hash.scm ends here
--- tests/hash.scm
+++ tests/hash.scm
@@ -37,10 +37,14 @@
   (base16-string->bytevector
    "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9"))
 
+(define %hello-sha512
+  (base16-string->bytevector
+   "309ecc489c12d6eb4cc40f50c902f2b4d0ed77ee511a7c7a9bcd3ca86d4cd86f989dd35bc5ff499670da34255b45b0cfd830e81f605dcf7dc5542e93ae9cd76f"))
+
 (define (supports-unbuffered-cbip?)
   "Return #t if unbuffered custom binary input ports (CBIPs) are supported.
 In Guile <= 2.0.9, CBIPs were always fully buffered, so the
-'open-sha256-input-port' does not work there."
+'open-hash-input-port' does not work there."
   (false-if-exception
    (setvbuf (make-custom-binary-input-port "foo" pk #f #f #f)
             (cond-expand ((and guile-2 (not guile-2.2)) _IONBF)
@@ -48,6 +52,17 @@ In Guile <= 2.0.9, CBIPs were always fully buffered, so the
 
 
 (test-begin "hash")
+
+(test-equal "lookup-hash-algorithm"
+  (hash-algorithm blake2b-512)
+  (lookup-hash-algorithm 'blake2b-512))
+
+(test-equal "hash-size"
+  (list 20 32 64)
+  (map hash-size
+       (list (hash-algorithm sha1)
+             (hash-algorithm sha256)
+             (hash-algorithm sha512))))
 
 (test-equal "sha1, empty"
   (base16-string->bytevector "da39a3ee5e6b4b0d3255bfef95601890afd80709")
@@ -65,6 +80,16 @@ In Guile <= 2.0.9, CBIPs were always fully buffered, so the
   %hello-sha256
   (sha256 (string->utf8 "hello world")))
 
+(test-equal "sha512, empty"
+  "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+  (bytevector->base16-string
+   (bytevector-hash #vu8() (hash-algorithm sha512))))
+
+(test-equal "sha512, hello"
+  %hello-sha512
+  (bytevector-hash (string->utf8 "hello world")
+                   (hash-algorithm sha512)))
+
 (test-equal "open-sha256-port, empty"
   %empty-sha256
   (let-values (((port get)
@@ -76,6 +101,14 @@ In Guile <= 2.0.9, CBIPs were always fully buffered, so the
   (list %hello-sha256 (string-length "hello world"))
   (let-values (((port get)
                 (open-sha256-port)))
+    (put-bytevector port (string->utf8 "hello world"))
+    (force-output port)
+    (list (get) (port-position port))))
+
+(test-equal "open-hash-port, sha512, hello"
+  (list %hello-sha512 (string-length "hello world"))
+  (let-values (((port get)
+                (open-hash-port (hash-algorithm sha512))))
     (put-bytevector port (string->utf8 "hello world"))
     (force-output port)
     (list (get) (port-position port))))
@@ -100,6 +133,16 @@ In Guile <= 2.0.9, CBIPs were always fully buffered, so the
   `("hello world" ,%hello-sha256)
   (let-values (((port get)
                 (open-sha256-input-port
+                 (open-bytevector-input-port
+                  (string->utf8 "hello world")))))
+    (let ((str (get-string-all port)))
+      (list str (get)))))
+
+(test-equal "open-hash-input-port, sha512, hello"
+  `("hello world" ,%hello-sha512)
+  (let-values (((port get)
+                (open-hash-input-port
+                 (hash-algorithm sha512)
                  (open-bytevector-input-port
                   (string->utf8 "hello world")))))
     (let ((str (get-string-all port)))
