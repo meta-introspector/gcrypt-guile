286d02d99c6b35da13d7b5f3ce68464e2eb80085
Ludovic Courtès <ludo@gnu.org>
2019-09-11 10:43:27 +0200

    Rename (gcrypt hmac) to (gcrypt mac).
    * gcrypt/hmac.scm: Rename to...
    * gcrypt/mac.scm: ... this.
    (ensure-mac-algorithm): Remove, and remove users.
    * gcrypt/hmac.scm: Rewrite as a shim above (gcrypt mac).
    * tests/hmac.scm: Rename to...
    * tests/mac.scm: ... this.  Remove uses of symbols to denote MAC
    algorithms.
    * tests/hmac.scm: Rewrite to test the old calling convention.
    * Makefile.am (SOURCES): Add gcrypt/mac.scm.
    (TESTS): Add tests/mac.scm.
    * guile-gcrypt.texi (Message Authentication Codes): Adjust module name
    and add deprecation notice.

--- gcrypt/hmac.scm
+++ gcrypt/hmac.scm
@@ -1,6 +1,4 @@
 ;;; guile-gcrypt --- crypto tooling for guile
-;;; Copyright © 2016 Christopher Allan Webber <cwebber@dustycloud.org>
-;;; Copyright © 2019 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;; Copyright © 2019 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of guile-gcrypt.
@@ -19,228 +17,43 @@
 ;;; along with guile-gcrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gcrypt hmac)
-  #:use-module (ice-9 format)
-  #:use-module (ice-9 match)
-  #:use-module (gcrypt base64)
-  #:use-module (gcrypt common)
-  #:use-module (gcrypt internal)
-  #:use-module (gcrypt random)
-  #:use-module (rnrs bytevectors)
-  #:use-module (system foreign)
-  #:export (mac-algorithm
-            lookup-mac-algorithm
-            mac-size
-
-            sign-data
+  #:use-module ((gcrypt mac) #:prefix mac:)
+  #:export (sign-data
             sign-data-base64
-            verify-sig verify-sig-base64
+            verify-sig
+            verify-sig-base64
             gen-signing-key))
 
-;;; HMAC
-;;; ====
+;;; Code:
+;;;
+;;; This module is deprecated and provided for compatibility with
+;;; Guile-Gcrypt 0.1.0.  Use (gcrypt mac) instead.
+;;;
+;;; Commentary:
 
-(define-syntax-rule (define-mac-algorithms name->integer
-                      symbol->integer mac-size
-                      (name id size) ...)
-  "Define hash algorithms with their NAME, numerical ID, and SIZE in bytes."
-  (begin
-    (define-enumerate-type name->integer symbol->integer
-      (name id) ...)
-
-    (define-lookup-procedure mac-size
-      "Return the size in bytes of a digest of the given hash algorithm."
-      (id size) ...)))
-
-(define-mac-algorithms mac-algorithm
-  lookup-mac-algorithm
-  mac-size
-
-  ;; GCRY_MAC_HMAC_*
-  (sha256 101 32)
-  (sha224 102 28)
-  (sha512 103 64)
-  (sha384 104 48)
-  (sha1 105 20)
-  (md5 106 16)
-  (md4 107 16)
-  (rmd160 108 20)
-  (tiger1 109 24)
-  (whirlpool 110 64)
-  (gostr3411-94 111 32)
-  (stribog256 112 32)
-  (stribog512 113 64)
-  ;; (md2 114 0)
-  (sha3-224 115 28)
-  (sha3-256 116 32)
-  (sha3-384 117 48)
-  (sha3-512 118 64))
-
-(define mac-algo-maclen
-  ;; This procedure was used to double-check the hash sizes above.  (We
-  ;; cannot use it at macro-expansion time because it wouldn't work when
-  ;; cross-compiling.)
-  (libgcrypt->procedure int "gcry_mac_get_algo_maclen" `(,int)))
-
-(define-inlinable (ensure-mac-algorithm obj)
-  ;; In Guile-Gcrypt 0.1.0, public procedures would accept a symbol to
-  ;; describe the algorithm.  This procedure is here to support this
-  ;; backward-compatibility.
-  (if (symbol? obj)
-      (lookup-mac-algorithm obj)
-      obj))
-
-(define %no-error 0)  ; GPG_ERR_NO_ERROR
-
-(define-wrapped-pointer-type <mac>
-  mac?
-  pointer->mac mac->pointer
-  (lambda (mac port)
-    (format port "#<mac ~x>"
-            (pointer-address (mac->pointer mac)))))
-
-(define %gcry-mac-open
-  (libgcrypt->procedure int "gcry_mac_open"
-                        ;; gcry_mac_hd_t *HD, int ALGO,
-                        ;; unsigned int FLAGS, gcry_ctx_t CTX
-                        `(* ,int ,unsigned-int *)))
-
-(define (mac-open algorithm)
-  "Create a <mac> object set to use ALGORITHM"
-  (let* ((mac (bytevector->pointer (make-bytevector (sizeof '*))))
-         (err (%gcry-mac-open mac algorithm 0 %null-pointer)))
-    (if (= err 0)
-        (pointer->mac (dereference-pointer mac))
-        (throw 'gcry-error 'mac-open err))))
-
-(define %gcry-mac-setkey
-  (libgcrypt->procedure int "gcry_mac_setkey" `(* * ,size_t)))
-
-(define (mac-setkey mac key)
-  "Set the KEY on <mac> object MAC
-
-In our case, KEY is either a string or a bytevector."
-  (let* ((key (match key
-                ((? bytevector? key)
-                 key)
-                ((? string? key)
-                 (string->utf8 key))))
-         (err (%gcry-mac-setkey (mac->pointer mac)
-                                (bytevector->pointer key)
-                                (bytevector-length key))))
-    (if (= err 0)
-        #t
-        (throw 'gcry-error 'mac-setkey err))))
-
-(define mac-close
-  (let ((proc (libgcrypt->procedure void
-                                    "gcry_mac_close"
-                                    '(*))))  ; gcry_mac_hd_t H
-    (lambda (mac)
-      "Release all resources of MAC.
-
-Running this on an already closed <mac> might segfault :)"
-      (proc (mac->pointer mac)))))
-
-(define mac-write
-  (let ((proc (libgcrypt->procedure int
-                                    "gcry_mac_write"
-                                    `(* * ,size_t))))
-    (lambda (mac obj)
-      "Writes string or bytevector OBJ to MAC"
-      (let* ((bv (match obj
-                   ((? bytevector? obj)
-                    obj)
-                   ((? string? obj)
-                    (string->utf8 obj))))
-             (err (proc (mac->pointer mac)
-                        (bytevector->pointer bv)
-                        (bytevector-length bv))))
-        (if (= err 0)
-            #t
-            (throw 'gcry-error 'mac-write err))))))
-
-(define mac-read
-  (let ((proc (libgcrypt->procedure int
-                                    "gcry_mac_read"
-                                    `(* * *))))
-    (lambda (mac algorithm)
-      "Get bytevector representing result of MAC's written, signed data"
-      (define (int-bv* n)
-        ;; Get the pointer to a bytevector holding an integer with this number
-        (let ((bv (make-bytevector (sizeof int))))
-          (bytevector-uint-set! bv 0 n (native-endianness) (sizeof int))
-          (bytevector->pointer bv)))
-      (let* ((bv-len (mac-size algorithm))
-             (bv (make-bytevector bv-len))
-             (err (proc (mac->pointer mac)
-                        (bytevector->pointer bv)
-                        (int-bv* bv-len))))
-        (if (= err 0)
-            bv
-            (throw 'gcry-error 'mac-read err))))))
-
-;; GPG_ERR_CHECKSUM *should* be 10, but it seems to return here as
-;; 16777226... unfortunately this is because we're pulling back an integer
-;; rather than the gcry_error_t type.
-
-(define mac-verify
-  (let ((proc (libgcrypt->procedure int
-                                    "gcry_mac_verify"
-                                    `(* * ,size_t))))
-    (lambda (mac bv)
-      "Verify that BV matches result calculated in MAC
-
-BV should be a bytevector with previously calculated data."
-      (let ((err (proc (mac->pointer mac)
-                       (bytevector->pointer bv)
-                       (bytevector-length bv))))
-        (if (= err 0)
-            (values #t err)
-            ;; TODO: This is WRONG!  See the comment above
-            ;;   this procedure's definition for why.  If we could
-            ;;   parse it as the appropriate GPG error, GPG_ERR_CHECKSUM
-            ;;   should be 10.
-            (values #f err))))))
-
-(define* (sign-data key data #:key
-                    (algorithm (mac-algorithm sha512)))
+(define* (sign-data key data #:key (algorithm 'sha512))
   "Signs DATA with KEY for ALGORITHM.  Returns a bytevector."
-  (let* ((algorithm (ensure-mac-algorithm algorithm))
-         (mac (mac-open algorithm)))
-    (mac-setkey mac key)
-    (mac-write mac data)
-    (let ((result (mac-read mac algorithm)))
-      (mac-close mac)
-      result)))
+  (mac:sign-data key data
+                 #:algorithm
+                 (mac:lookup-mac-algorithm algorithm)))
 
-(define* (sign-data-base64 key data #:key
-                           (algorithm (mac-algorithm sha512)))
-  "Like sign-data, but conveniently encodes to base64."
-  (let ((algorithm (ensure-mac-algorithm algorithm)))
-    (base64-encode (sign-data key data #:algorithm algorithm))))
+(define* (sign-data-base64 key data #:key (algorithm 'sha512))
+  "Signs DATA with KEY for ALGORITHM.  Returns a bytevector."
+  (mac:sign-data-base64 key data
+                        #:algorithm
+                        (mac:lookup-mac-algorithm algorithm)))
 
-
-;; @@: Shouldn't this be "valid-sig?"
-(define* (verify-sig key data sig
-                     #:key (algorithm (mac-algorithm sha512)))
+(define* (verify-sig key data sig #:key (algorithm 'sha512))
   "Verify that DATA with KEY matches previous signature SIG for ALGORITHM."
-  (let* ((algorithm (ensure-mac-algorithm algorithm))
-         (mac (mac-open algorithm)))
-    (mac-setkey mac key)
-    (mac-write mac data)
-    (let ((result (mac-verify mac sig)))
-      (mac-close mac)
-      result)))
+  (mac:verify-sig key data sig
+                  #:algorithm
+                  (mac:lookup-mac-algorithm algorithm)))
 
-(define* (verify-sig-base64 key data b64-sig
-                            #:key (algorithm (mac-algorithm sha512)))
-  (verify-sig key data
-               (base64-decode b64-sig)
-               #:algorithm algorithm))
+(define* (verify-sig-base64 key data sig #:key (algorithm 'sha512))
+  "Verify that DATA with KEY matches previous signature SIG for ALGORITHM."
+  (mac:verify-sig-base64 key data sig
+                         #:algorithm
+                         (mac:lookup-mac-algorithm algorithm)))
 
-(define* (gen-signing-key #:optional (key-length 128))
-  "Generate a signing key (a bytevector).
-
-KEY-LENGTH is the length, in bytes, of the key.  The default is 128.
-This should be a multiple of 8."
-  (gen-random-bv key-length %gcry-very-strong-random))
+(define gen-signing-key
+  mac:gen-signing-key)
