meta-introspector/gcrypt-guile

commit cf8a01a2a7df66828a6b5c7e10da4045336f940b

Move 'libgcrypt->pointer' & co. to (gcrypt internal).
* gcrypt/common.scm (libgcrypt->pointer, libgcrypt->procedure): Move to.
* gcrypt/internal.scm: ... here.
* gcrypt/hash.scm, gcrypt/mac.scm, gcrypt/pk-crypto.scm,
gcrypt/random.scm: Adjust accordingly.

gcrypt/common.scm
@@ -1,5 +1,5 @@
 ;;; guile-gcrypt --- crypto tooling for guile
-;;; Copyright © 2013, 2014, 2015 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013, 2014, 2015, 2019 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2019 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;;
 ;;; This file is part of guile-gcrypt.
@@ -18,12 +18,10 @@
 ;;; along with guile-gcrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gcrypt common)
-  #:use-module (gcrypt package-config)
+  #:use-module (gcrypt internal)
   #:use-module (system foreign)
   #:use-module (ice-9 match)
   #:export (gcrypt-version
-            libgcrypt->pointer
-            libgcrypt->procedure
             error-source error-string))
 
 ;;; Commentary:
@@ -32,29 +30,6 @@
 ;;; initializes Libgcrypt as a side effect.
 ;;;
 ;;; Code:
-
-(define (libgcrypt->pointer name)
-  "Return a pointer to symbol FUNC in libgcrypt."
-  (catch #t
-    (lambda ()
-      (dynamic-func name (dynamic-link %libgcrypt)))
-    (lambda args
-      (lambda _
-        (throw 'system-error name  "~A" (list (strerror ENOSYS))
-               (list ENOSYS))))))
-
-(define (libgcrypt->procedure return name params)
-  "Return a pointer to symbol FUNC in libgcrypt."
-  (catch #t
-    (lambda ()
-      (let ((ptr (dynamic-func name (dynamic-link %libgcrypt))))
-        ;; The #:return-errno? facility was introduced in Guile 2.0.12.
-        (pointer->procedure return ptr params
-                            #:return-errno? #t)))
-    (lambda args
-      (lambda _
-        (throw 'system-error name  "~A" (list (strerror ENOSYS))
-               (list ENOSYS))))))
 
 (define gcrypt-version
   ;; According to the manual, this function must be called before any other,

gcrypt/internal.scm
@@ -17,7 +17,12 @@
 ;;; along with guile-gcrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gcrypt internal)
-  #:export (define-enumerate-type
+  #:use-module (gcrypt package-config)
+  #:use-module (system foreign)
+  #:export (libgcrypt->pointer
+            libgcrypt->procedure
+
+            define-enumerate-type
             define-lookup-procedure))
 
 ;;; Code:
@@ -26,6 +31,29 @@
 ;;; change anytime; you should not rely on it.
 ;;;
 ;;; Comment:
+
+(define (libgcrypt->pointer name)
+  "Return a pointer to symbol FUNC in libgcrypt."
+  (catch #t
+    (lambda ()
+      (dynamic-func name (dynamic-link %libgcrypt)))
+    (lambda args
+      (lambda _
+        (throw 'system-error name  "~A" (list (strerror ENOSYS))
+               (list ENOSYS))))))
+
+(define (libgcrypt->procedure return name params)
+  "Return a pointer to symbol FUNC in libgcrypt."
+  (catch #t
+    (lambda ()
+      (let ((ptr (dynamic-func name (dynamic-link %libgcrypt))))
+        ;; The #:return-errno? facility was introduced in Guile 2.0.12.
+        (pointer->procedure return ptr params
+                            #:return-errno? #t)))
+    (lambda args
+      (lambda _
+        (throw 'system-error name  "~A" (list (strerror ENOSYS))
+               (list ENOSYS))))))
 
 (define-syntax-rule (define-enumerate-type name->integer symbol->integer
                       (name id) ...)

gcrypt/hash.scm
@@ -18,7 +18,6 @@
 ;;; along with guile-gcrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gcrypt hash)
-  #:use-module (gcrypt common)
   #:use-module (gcrypt utils)
   #:use-module (gcrypt internal)
   #:use-module (rnrs bytevectors)

gcrypt/mac.scm
@@ -22,7 +22,6 @@
   #:use-module (ice-9 format)
   #:use-module (ice-9 match)
   #:use-module (gcrypt base64)
-  #:use-module (gcrypt common)
   #:use-module (gcrypt internal)
   #:use-module (gcrypt random)
   #:use-module (rnrs bytevectors)

gcrypt/pk-crypto.scm
@@ -19,8 +19,8 @@
 
 (define-module (gcrypt pk-crypto)
   #:use-module (gcrypt base16)
+  #:use-module (gcrypt internal)
   #:use-module (gcrypt common)
-
   #:use-module (system foreign)
   #:use-module (rnrs bytevectors)
   #:use-module (ice-9 match)

gcrypt/random.scm
@@ -18,7 +18,7 @@
 ;;; along with guile-gcrypt.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gcrypt random)
-  #:use-module (gcrypt common)
+  #:use-module (gcrypt internal)
   #:use-module (gcrypt base64)
   #:use-module (rnrs bytevectors)
   #:use-module (system foreign)

commit bdff239141f2359fd35211ff0efd0e36829b257d

internal: Ensure 'gcry_check_version' is call upfront.
Fixes <https://bugs.gnu.org/37616>.
Reported by Hartmut Goebel <h.goebel@crazy-compilers.com>.

The regression was in part introduced by
cf8a01a2a7df66828a6b5c7e10da4045336f940b.

* gcrypt/common.scm: Re-export 'gcrypt-version'.
(gcrypt-version): Move to...
* gcrypt/internal.scm (gcrypt-version): ... here.  Call PROC from the
top level and catch 'system-error' exceptions during that call.

gcrypt/common.scm
@@ -21,8 +21,8 @@
   #:use-module (gcrypt internal)
   #:use-module (system foreign)
   #:use-module (ice-9 match)
-  #:export (gcrypt-version
-            error-source error-string))
+  #:re-export (gcrypt-version)
+  #:export (error-source error-string))
 
 ;;; Commentary:
 ;;;
@@ -30,15 +30,6 @@
 ;;; initializes Libgcrypt as a side effect.
 ;;;
 ;;; Code:
-
-(define gcrypt-version
-  ;; According to the manual, this function must be called before any other,
-  ;; and it's not clear whether it can be called more than once.  So call it
-  ;; right here from the top level.
-  (let ((proc (libgcrypt->procedure '* "gcry_check_version" '(*))))
-    (lambda ()
-      "Return the version number of libgcrypt as a string."
-      (pointer->string (proc %null-pointer)))))
 
 (define error-source
   (let ((proc (libgcrypt->procedure '* "gcry_strsource" (list int))))

gcrypt/internal.scm
@@ -23,7 +23,9 @@
             libgcrypt->procedure
 
             define-enumerate-type
-            define-lookup-procedure))
+            define-lookup-procedure
+
+            gcrypt-version))
 
 ;;; Code:
 ;;;
@@ -94,3 +96,17 @@ value in O(1)."
                  (and (<= integer #,max) (>= integer #,min)
                       (let ((result (vector-ref values (- integer #,min))))
                         (and (> result 0) result)))))))))))
+
+(define gcrypt-version
+  ;; According to the manual, this function must be called before any other,
+  ;; and it's not clear whether it can be called more than once.  So call it
+  ;; right here from the top level.  During cross-compilation, the call to
+  ;; PROC fails with a 'system-error exception; catch it.
+  (let* ((proc    (libgcrypt->procedure '* "gcry_check_version" '(*)))
+         (version (catch 'system-error
+                    (lambda ()
+                      (pointer->string (proc %null-pointer)))
+                    (const ""))))
+    (lambda ()
+      "Return the version number of libgcrypt as a string."
+      version)))